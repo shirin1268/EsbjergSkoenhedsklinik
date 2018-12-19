31f98de6e43d6c0ed877dfe52f8d4b62e05bd58e
Shirin Vazirian <Shirin1268@gmail.com>
2018-12-19 18:25:42 +0100

    try to resolve konflikt

--- createJournal.php
+++ createJournal.php
@@ -7,51 +7,6 @@ $jh = new JournalHandler();
 $pa = new PatientAccountHandler();
 $fh =new FormHandler();
 $encrypt= new Encryption();
-?>
-
-    <div class="col-md-auto" style="min-width: 80%">
-<form id="createJ" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-<h4>Opret journal til patienten via nedenstående form</h4>
-    <table class='table table-hover table-responsive table-bordered'>
-
-        <tr>
-            Find patientens CPR
-                <select class='form-control' name='cpr' >
-                    <?php
-                    $fh->readCPR();
-                    ?>
-                </select>
-        </tr><br>
-        <tr>
-            Behandlingsdato:
-            <br>
-            <input type='date' name='dato' class='form-control' >
-        </tr><br>
-        <tr>
-            Behandlingsnavn:
-            <input type='text' name='behandlingname' class='form-control' />
-        </tr><br>
-
-        <tr>
-            Beskrivelse af behandlingen:
-            <textarea type='text' name='description' class="form-control" id="exampleFormControlTextarea1" rows="10" ></textarea>
-        </tr><br>
-
-        <tr>
-            Betaling
-            <textarea name='betaling' class='form-control'></textarea>
-        </tr><br>
-
-        <tr>
-                <button type="submit" name="CreateJournal" class="btn btn-login float-right">Create Journal</button>
-        </tr><br><br>
-
-    </table><br>
-</form>
-
-</div>
-
-<?php
 if (isset($_POST['CreateJournal']))
 {
     if(
@@ -77,6 +32,52 @@ if (isset($_POST['CreateJournal']))
     }
 
 }
+?>
+
+    <div class="col-md-auto" style="min-width: 80%">
+        <form id="createJ" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+            <h4>Opret journal til patienten via nedenstående form</h4>
+            <table class='table table-hover table-responsive table-bordered'>
+
+                <tr>
+                    Find patientens CPR
+                    <select class='form-control' name='cpr' >
+                        <?php
+                        $fh->readCPR();
+                        ?>
+                    </select>
+                </tr><br>
+                <tr>
+                    Behandlingsdato:
+                    <br>
+                    <input type='date' name='dato' class='form-control' >
+                </tr><br>
+                <tr>
+                    Behandlingsnavn:
+                    <input type='text' name='behandlingname' class='form-control' />
+                </tr><br>
+
+                <tr>
+                    Beskrivelse af behandlingen:
+                    <textarea type='text' name='description' class="form-control" id="exampleFormControlTextarea1" rows="10" ></textarea>
+                </tr><br>
+
+                <tr>
+                    Betaling
+                    <textarea name='betaling' class='form-control'></textarea>
+                </tr><br>
+
+                <tr>
+                    <button type="submit" name="CreateJournal" class="btn btn-login float-right">Create Journal</button>
+                </tr><br><br>
+
+            </table><br>
+        </form>
+
+    </div>
+
+<?php
+
 
 include_once "footer.php";
 ?>
