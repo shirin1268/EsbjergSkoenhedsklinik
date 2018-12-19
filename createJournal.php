4fff4edc4cae5dda07f441e6a0ba5ec78db44d3a
Shirin Vazirian <Shirin1268@gmail.com>
2018-12-19 19:00:34 +0100

    test

--- createJournal.php
+++ createJournal.php
@@ -21,19 +21,26 @@ if (isset($_POST['CreateJournal']))
         $betaling =ValidateHandler::validinput($_POST["betaling"],$connection);
         $dato=ValidateHandler::validinput($_POST['dato'],$connection);
         $cpr = $encrypt->encrypt_decrypt('encrypt',trim($_POST["cpr"]));
-
-    if($jh->createJournal($behandlingname,$description,$dato,$betaling,$cpr)==true)
-    {
-        echo "<div class='alert alert-success'>Journal was created.</div>";
+        if($jh->createJournal($behandlingname,$description,$dato,$betaling,$cpr)==true)
+        {
+            echo '<div class="alert alert-success alert-dismissible fade show" id="alert" role="alert">
+                Journal was created.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>';
+        }
+        else{
+            echo '<div class="alert alert-danger alert-dismissible fade show" id="alert" role="alert">
+                Unable to create Journal.
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>';
+        }
     }
-    else{
-        echo "<div class='alert alert-danger'>Unable to create Journal.</div>";
-    }
-    }
-
 }
 ?>
-
     <div class="col-md-auto" style="min-width: 80%">
         <form id="createJ" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
             <h4>Opret journal til patienten via nedenstående form</h4>
@@ -76,8 +83,33 @@ if (isset($_POST['CreateJournal']))
 
     </div>
 
+    <!-- <form id="createJ" action="<?php // echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <div class="form-group">
+        <label>CPR nr.:</label>
+        <select class='form-control' name='cpr' >
+            <?php
+//  $fh->readCPR();
+?>
+        </select>
+    </div>
+    <div class="form-group">
+        <label>Behandlingsdato:</label>
+        <input type='date' name='dato' class='form-control' >
+    </div>
+    <div class="form-group">
+        <label>Behandling:</label>
+        <input type='text' name='behandlingname' class='form-control' />
+    </div>
+    <div class="form-group">
+        <label>Beskrivelse:</label>
+        <textarea type='text' name='description' class="form-control" id="exampleFormControlTextarea1" rows="10" ></textarea>
+    </div>
+    <div class="form-group">
+        <label>Betaling:</label>
+        <textarea name='betaling' class='form-control'></textarea>
+    </div>
+    <button type="submit" name="CreateJournal" class="btn btn-login float-right">Create Journal</button>
+</form>  __>
 <?php
-
-
 include_once "footer.php";
 ?>
