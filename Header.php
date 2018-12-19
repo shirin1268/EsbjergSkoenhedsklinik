8487310e80ce8f5aab9b2f83bee1dc8c6ea02c02
drbeat <xgecco@gmail.com>
2018-12-19 11:35:09 +0100

    fontawesome css correction

--- Header.php
+++ Header.php
@@ -16,7 +16,7 @@ $ah = new AdminHandler();
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title><?php echo $page_title; ?> </title>
-    <link rel="stylesheet" href="assets/fontawesome/all.min.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 
     <link rel="stylesheet" href="Css/stylesheet.css" />
